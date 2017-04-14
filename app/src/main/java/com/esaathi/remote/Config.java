02bd00fa7f04eeac0082d75b6cec92dfa18c250f
Rohan Garg <dev.RohanGarg@gmail.com>
2017-04-14 22:58:11 +0530

    feat: Update config URL and show title in toolbar

--- app/src/main/java/com/esaathi/remote/Config.java
+++ app/src/main/java/com/esaathi/remote/Config.java
@@ -8,11 +8,13 @@ import java.net.URL;
 public class Config {
     public static final URL BASE_URL;
     private static final String TAG = "Config";
+    private static final String KEY = "AIzaSyDYPZ98uUm49hXsI9-I5Dk7Aqc4GnRWWLQ";
+    private static final String KIDS_PLAYLIST_ID = "PLTNAiZtt-h0tNhd9fRActCB3MrOOv0r1V";
 
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json");
+            url = new URL("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=" + KEY + "&playlistId=" + KIDS_PLAYLIST_ID);
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Error creating an URL. Incorrect specification.", ignored);
