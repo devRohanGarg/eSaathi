a1b7ba552dbd5c06c0ba55c10bf3f86bbb8bc4ac
Rohan Garg <dev.RohanGarg@gmail.com>
2017-04-15 04:49:15 +0530

    feat: Add category images and text

--- app/src/main/java/com/esaathi/ui/CategoryActivity.java
+++ app/src/main/java/com/esaathi/ui/CategoryActivity.java
@@ -1,5 +1,6 @@
 package com.esaathi.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,5 +12,13 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(getApplicationContext(), AgeGroupActivity.class));
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_right_out);
+        finish();
     }
 }
