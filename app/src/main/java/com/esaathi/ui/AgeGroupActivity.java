ee5c03b2c4139b32d1e664daa01310a80c379c87
Rohan Garg <dev.RohanGarg@gmail.com>
2017-04-15 00:48:20 +0530

    feat: Add age group activity

---
+++ app/src/main/java/com/esaathi/ui/AgeGroupActivity.java
@@ -0,0 +1,16 @@
+package com.esaathi.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esaathi.R;
+
+public class AgeGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_age_group);
+
+    }
+}
--- app/src/main/java/com/esaathi/ui/WelcomeActivity.java
+++ app/src/main/java/com/esaathi/ui/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.esaathi.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), AgeGroupActivity.class));
                 overridePendingTransition(R.anim.push_right_in, R.anim.push_left_out);
                 finish();
             }
