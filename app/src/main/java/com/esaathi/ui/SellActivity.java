167da8d223a050c535d87036d67cebbebd15aed3
Rohan Garg <dev.RohanGarg@gmail.com>
2017-04-15 05:37:38 +0530

    feat: Implement sell activity

--- app/src/main/java/com/esaathi/ui/SellActivity.java
+++ app/src/main/java/com/esaathi/ui/SellActivity.java
@@ -1,17 +1,27 @@
 package com.esaathi.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.esaathi.R;
 
-public class SellActivity extends AppCompatActivity {
+import im.delight.android.webview.AdvancedWebView;
+
+public class SellActivity extends AppCompatActivity implements AdvancedWebView.Listener {
+
+    private AdvancedWebView mWebView;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
+        mWebView = (AdvancedWebView) findViewById(R.id.form);
+        mWebView.setListener(this, this);
+        mWebView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSfPHYGiiRZHob_Oqj8UG36o8B1cnnei980yesFxERvFxTM-9A/viewform?c=0&w=1");
     }
 
     @Override
@@ -20,5 +30,31 @@ public class SellActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), AgeGroupActivity.class));
         overridePendingTransition(R.anim.push_left_in, R.anim.push_right_out);
         finish();
+    }
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+        dialog = ProgressDialog.show(this, "", "Loading. Please wait...", true);
+    }
+
+    @Override
+    public void onPageFinished(String url) {
+        if (dialog != null)
+            dialog.dismiss();
+    }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+
+    }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
+
+    }
+
+    @Override
+    public void onExternalPageRequest(String url) {
+
     }
 }
